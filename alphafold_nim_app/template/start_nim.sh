6ad0858be9e79d0084a98c23ffd284667864d7d2
mcuma <m.cuma@utah.edu>
2025-05-02 14:10:56 -0600

    add Alphafold NIM app

--- alphafold_nim_app/template/start_nim.sh
+++ alphafold_nim_app/template/start_nim.sh
@@ -10,4 +10,4 @@ else
 fi
 export APPTAINER_DOCKER_PASSWORD=$NGC_API_KEY
 
-apptainer run --nv -e -B /scratch/general/vast/u0101881/nvs:/home/nvs -B /scratch/general/vast/u0101881/nim:/opt/nim/.cache -B /scratch/general/vast/u0101881/nim/af2/workspace:/opt/nim/workspace --env NGC_API_KEY=$NGC_API_KEY,DOCKER_USERNAME='\$oauthtoken',NIM_HTTP_API_PORT=8081,CUDA_VISIBLE_DEVICES=0,NIM_PARALLEL_MSA_RUNNERS=3,NIM_PARALLEL_THREADS_PER_MSA=12,NIM_CACHE_PATH=/opt/nim/.cache /uufs/chpc.utah.edu/common/home/u0101881/nvidia/nim/alphafold2_2.1.sif
+apptainer run --nv -e -B /scratch/general/vast/$USER/nvs:/home/nvs -B /scratch/general/vast/u0101881/nim:/opt/nim/.cache -B /scratch/general/vast/u0101881/nim/af2/workspace:/opt/nim/workspace --env NGC_API_KEY=$NGC_API_KEY,DOCKER_USERNAME='\$oauthtoken',NIM_HTTP_API_PORT=8081,CUDA_VISIBLE_DEVICES=0,NIM_PARALLEL_MSA_RUNNERS=3,NIM_PARALLEL_THREADS_PER_MSA=12,NIM_CACHE_PATH=/opt/nim/.cache /uufs/chpc.utah.edu/common/home/u0101881/nvidia/nim/alphafold2_2.1.sif
