fa7377769d04a3df14efe025d25accf7fb4df039
MikeUU332 <michael.ramshaw@hsc.utah.edu>
2025-05-27 16:56:18 -0600

    Added new download_dir param

--- vllm_app/template/start_vllm_server.sh
+++ vllm_app/template/start_vllm_server.sh
@@ -3,14 +3,19 @@
 echo Starting vLLM on port $1
 ml apptainer
 set -x
-#export APPTAINER_DOCKER_USERNAME='$oauthtoken'
-# if [ -z "${1}" ]; then
-#  export NGC_API_KEY=$2
-#else
-#  export NGC_API_KEY=ajMzaHZmMDdmZzNjYXNvanYzOG41NTZpOHU6MDM5NjY4ZGItMjQxYS00ODk5LTg3NWMtMGVjZGI1NDcxMjFl
-#fi
-export SERVER_PORT=$1
-export LLM_MODEL=$2
-export HUGGINGFACE_TOKEN=$3
-# see https://docs.nvidia.com/nim/large-language-models/latest/configuration.html
-apptainer run --nv -e -B /scratch/general/vast/$USER/vllm_cache:/root/.cache/huggingface  /uufs/chpc.utah.edu/sys/installdir/r8/vllm/vllm-0.8.5p1.sif --host=localhost --port=$SERVER_PORT --model=$LLM_MODEL --hf-token=$HUGGINGFACE_TOKEN
+
+#need to create user-specified scratch dir if it does not already exist
+echo $SCRATCH_DIR
+# mkdir -p $SCRATCH_DIR
+# export HUGGINGFACE_HUB_CACHE=~/hh_cache
+# mkdir -p $HUGGINGFACE_HUB_CACHE
+# Note we have to set HF_HOME to the directory inside the container which then is bound outside
+
+# Below line works with model=NousResearch/Meta-Llama-3-8B-Instruct
+# and SCRATCH_DIR=/scratch/general/vast/u6040150/vllm_cache
+
+# PROBLEM - so far there does not seem to be a one size fits all way to get VLLM to use a specified
+# cache dir. This snippet works for HF auth models but not others:
+# --env HF_HOME=/root/.cache/huggingface --bind ${SCRATCH_DIR}:/root/.cache/huggingface
+apptainer run --nv -e --fakeroot --env HF_HOME=/root/.cache/ --bind ${SCRATCH_DIR}:/root/.cache  /uufs/chpc.utah.edu/sys/installdir/r8/vllm/vllm-0.8.5p1.sif --host=localhost --port=$nimport --model=$MODEL --hf-token=$HUGGINGFACE_TOKEN --download_dir=$SCRATCH_DIR
+
