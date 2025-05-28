4b01739dc123461b0fd07175f6dd6400c5e2ac69
MikeUU332 <michael.ramshaw@hsc.utah.edu>
2025-05-28 10:20:05 -0600

    scripts to handle new task widget

--- vllm_app/template/start_vllm_server.sh
+++ vllm_app/template/start_vllm_server.sh
@@ -5,7 +5,7 @@ ml apptainer
 set -x
 
 #need to create user-specified scratch dir if it does not already exist
-echo $SCRATCH_DIR
+echo $SCRATCH_DIR $TASK_CHOICE
 # mkdir -p $SCRATCH_DIR
 # export HUGGINGFACE_HUB_CACHE=~/hh_cache
 # mkdir -p $HUGGINGFACE_HUB_CACHE
@@ -17,5 +17,5 @@ echo $SCRATCH_DIR
 # PROBLEM - so far there does not seem to be a one size fits all way to get VLLM to use a specified
 # cache dir. This snippet works for HF auth models but not others:
 # --env HF_HOME=/root/.cache/huggingface --bind ${SCRATCH_DIR}:/root/.cache/huggingface
-apptainer run --nv -e --fakeroot --env HF_HOME=/root/.cache/ --bind ${SCRATCH_DIR}:/root/.cache  /uufs/chpc.utah.edu/sys/installdir/r8/vllm/vllm-0.8.5p1.sif --host=localhost --port=$nimport --model=$MODEL --hf-token=$HUGGINGFACE_TOKEN --download_dir=$SCRATCH_DIR
+apptainer run --nv -e --fakeroot --env HF_HOME=/root/.cache/ --bind ${SCRATCH_DIR}:/root/.cache  /uufs/chpc.utah.edu/sys/installdir/r8/vllm/vllm-0.8.5p1.sif --host=localhost --port=$nimport --model=$MODEL --hf-token=$HUGGINGFACE_TOKEN --download_dir=$SCRATCH_DIR --task=$TASK_CHOICE
 
