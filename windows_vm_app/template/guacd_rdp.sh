c02c496434f3441edfcede455a403b6aa52b95ed
Michael Ramshaw <u6040150@notchpeak2.int.chpc.utah.edu>
2025-11-25 16:56:02 -0700

    fixed Windows VM startup problem

--- windows_vm_app/template/guacd_rdp.sh
+++ windows_vm_app/template/guacd_rdp.sh
@@ -4,13 +4,14 @@ module load apptainer/1.4.1
 module load nodejs/22.4.0
 
 GUAC_DIR="/uufs/chpc.utah.edu/sys/installdir/r8/apache_guacamole"
-export API_SESSION_TIMEOUT=5
-export SESSION_TIMEOUT=5
+export API_SESSION_TIMEOUT=600
+export SESSION_TIMEOUT=600
 echo "GUACD_RDP.SH:  launching guacd:..."
+apptainer run ${GUAC_DIR}/guacd_latest.sif &
 # Load in a while loop so if the container is killed, another one will start up
-( while true; do apptainer run ${GUAC_DIR}/guacd_latest.sif; sleep 1; done ) &
+#( while true; do apptainer run ${GUAC_DIR}/guacd_latest.sif; sleep 1; done ) &
 # Load in a while loop so if 7lbd_server.js is killed, another one will start up
 echo "GUACD_RDP.SH:  Starting Guacd RDP Connector..."
-( while true; do node ${GUAC_DIR}/guacd_connector.js guacd_rdp.json >> guacd_log; sleep 1; done ) & 
-#node "${GUAC_DIR}/guacd_connector.js" guacd_rdp.json > "$log_file" 2>&1 &
+node ${GUAC_DIR}/guacd_connector.js guacd_rdp.json >> guacd_log &
+#( while true; do node ${GUAC_DIR}/guacd_connector.js guacd_rdp.json >> guacd_log; sleep 1; done ) & 
 
