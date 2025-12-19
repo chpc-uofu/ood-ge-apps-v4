1cd4f3526ef9549c3f6edc16851cb71805bea74e
Michael Ramshaw <u6040150@notchpeak1.int.chpc.utah.edu>
2025-12-18 18:38:15 -0700

    now do graceful samba server shutdown

--- windows_vm_app/template/clean.sh
+++ windows_vm_app/template/clean.sh
@@ -1,3 +1,7 @@
+# shutdown samba server cleanly so temp files can be removed
+# Samba says "The safe way to terminate an smbd is to send it a SIGTERM (-15) signal and wait for it to die on its own"
+smbpid=$(ps aux | grep smbd | grep -v grep | awk '{print $2}')
+kill -15 $smbpid
 #Clean up temporary directory on job exit.
 rm -r ${JOB_TMP_DIR}
 
