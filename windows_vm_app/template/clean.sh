3d86e4e3a90f01c60efa7862878a9d1a38ad0a61
Michael Ramshaw <u6040150@notchpeak1.int.chpc.utah.edu>
2026-01-02 16:31:42 -0700

    samba server diagnosing

--- windows_vm_app/template/clean.sh
+++ windows_vm_app/template/clean.sh
@@ -1,7 +1,9 @@
 # shutdown samba server cleanly so temp files can be removed
 # Samba says "The safe way to terminate an smbd is to send it a SIGTERM (-15) signal and wait for it to die on its own"
+echo "Shutting down samba server"
 smbpid=$(ps aux | grep smbd | grep -v grep | awk '{print $2}')
 kill -15 $smbpid
+echo "Samba server should now be shut down"
 #Clean up temporary directory on job exit.
 rm -r ${JOB_TMP_DIR}
 
