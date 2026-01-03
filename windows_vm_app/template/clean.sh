ab5c5633336eb50a91d24601daaed328bbc5761b
Michael Ramshaw <u6040150@notchpeak1.int.chpc.utah.edu>
2026-01-02 18:52:01 -0700

    changing ps list to space separated instead of \n

--- windows_vm_app/template/clean.sh
+++ windows_vm_app/template/clean.sh
@@ -1,12 +1,13 @@
 # shutdown samba server cleanly so temp files can be removed
 # Samba says "The safe way to terminate an smbd is to send it a SIGTERM (-15) signal and wait for it to die on its own"
 echo "Shutting down samba server"
-smbpid=$(ps aux | grep smbd | grep -v grep | awk '{print $2}')
+smbpid=$(ps aux | grep smbd | grep -v grep | awk '{print $2}' | tr '\n' ' ')
 echo "smbpid is ${smbpid}"
 for p in ${smbpid}; do
    kill -15 $p
    echo "killing ${p}"
    while [ -e /proc/$p ]; do sleep 1; done
+   echo "killed ${p}"
 done
 echo "Samba server should now be shut down"
 #Clean up temporary directory on job exit.
