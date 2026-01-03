7b187224c1c9b9b9cfaa8ca6e68d8cf999029137
Michael Ramshaw <u6040150@notchpeak1.int.chpc.utah.edu>
2026-01-02 17:11:23 -0700

    waiting for a pid is the hardest part

--- windows_vm_app/template/clean.sh
+++ windows_vm_app/template/clean.sh
@@ -2,8 +2,9 @@
 # Samba says "The safe way to terminate an smbd is to send it a SIGTERM (-15) signal and wait for it to die on its own"
 echo "Shutting down samba server"
 smbpid=$(ps aux | grep smbd | grep -v grep | awk '{print $2}')
+echo "smbpid is ${smbpid}"
 kill -15 $smbpid
-wait $smbpid
+while [ -e /proc/$smbpid ];do sleep 1; done
 echo "Samba server should now be shut down"
 #Clean up temporary directory on job exit.
 rm -r ${JOB_TMP_DIR}
